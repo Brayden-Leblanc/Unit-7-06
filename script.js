f255279af8befe148fc25203d6c3a20b2c6420ed
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-23 15:49:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Turns userInput into a variable
+document.getElementById('button').addEventListener('click', age)
+let myage = 15
+let answer = 0
+
+function age () {
+
+  do {
+  answer = prompt('What is my age?", "Enter your guess here')
+  answer = parseInt(answer)
+    if (myage === answer) {
+    alert('Congrats! You won!')
+  } else if (myage < answer) {
+    alert('You have to go lower than that!')
+  } else if (myage > answer) {
+    alert('You have to go higher than that!')
+  } else {
+    alert('That is not a valid response (use only positive numbers, no letters, or negative numbers)')
+  }
+  } while(myage != answer)
+}
